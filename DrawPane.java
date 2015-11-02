cfbc2e46dc3b427261ce21e06a08d2b846aa938e
XuanyiZhu <xzhu2@uvm.edu>
2015-11-01 20:05:15 -0500

    Update DrawPane.java

--- DrawPane.java
+++ DrawPane.java
@@ -7,8 +7,6 @@
  * @author Xuanyi Zhu
  */
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.Background;
@@ -16,7 +14,7 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public abstract class DrawPane extends Application {
@@ -25,6 +23,7 @@ public abstract class DrawPane extends Application {
     static final int PANE_Y_DIM = 800;
     Pane pane;
     private Scene scene;
+    Line line;
 
     /**
      * @return the PANE_X_DIM
@@ -40,13 +39,17 @@ public abstract class DrawPane extends Application {
         return PANE_Y_DIM;
     }
 
+    /**
+     * draw a pane and set background color by input a color
+     *
+     * @param backgroundColor
+     */
     public DrawPane(Color backgroundColor) {
         pane = new Pane();
         BackgroundFill myBF = new BackgroundFill(backgroundColor, new CornerRadii(1),
                 new Insets(0.0, 0.0, 0.0, 0.0));// or null for the padding
         pane.setBackground(new Background(myBF));
         scene = new Scene(pane, PANE_X_DIM, PANE_Y_DIM);
-
     }
 
     @Override
@@ -55,13 +58,5 @@ public abstract class DrawPane extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    protected void setUpAnimation() {
-        // Create a handler
-        EventHandler<ActionEvent> eventHandler = (ActionEvent e) -> {
-            update();
-        };
-    }
-
-    abstract public void update();
+    
 }
